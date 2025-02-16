eb3cc934e45404c96b6b364e1715749783688305
YashRaj-I <yr7971556@gmail.com>
2025-02-16 08:59:56 +0530

    add project

---
+++ main.py
@@ -0,0 +1,68 @@
+# pip install speechrecognition pyaudio
+# pip install setuptools
+# pip install pyttsx3
+# pip install pocketsphinx
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+import musiclibrary
+
+
+
+recognizer = sr.Recognizer()
+engine = pyttsx3.init()
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def processcommand(c):
+    if "open google" in c.lower():
+        webbrowser.open("https://google.com")
+    elif "open facebook" in c.lower():
+        webbrowser.open("https://facebook.com")
+    elif "open youtube" in c.lower():
+        webbrowser.open("https://youtube.com")
+    elif "open linkedin" in c.lower():
+        webbrowser.open("https://linkedin.com")
+    elif c.lower().startswith("play"):
+        song =c.lower().split(" ")[1]
+        link = musiclibrary.music[song]
+        webbrowser.open(link)
+    else:
+        # let OpenAI handle the request
+        pass
+
+if __name__ =="__main__":
+    speak("Initializing radha...")
+    while True:
+        # listen for the wake world "jarvis"
+        # obtain audio from the microphone
+        r =sr.Recognizer()
+
+        print("recognizing..")
+
+        # recognize speech using sphinx
+        try:
+            with sr.Microphone() as source:
+                print("Listining...")
+                audio = r.listen(source , timeout=2, phrase_time_limit=2)
+            command = r.recognize_google(audio)
+            if (command.lower() == "radha"):
+                speak("Ya")
+                # listen this command
+                with sr.Microphone() as source:
+                    print("radha Active...")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+                    processcommand(command)
+
+        except Exception as e:
+            print("Error; {0}".format(e))
+
+
+
+
+
+
+
---
+++ musiclibrary.py
@@ -0,0 +1,7 @@
+music ={
+    "skyfall": "https://www.youtube.com/watch?v=sZrTJesvJeo",
+    "beliver": "https://www.youtube.com/watch?v=W0DM5lcj6mw",
+    "bones" : "https://www.youtube.com/watch?v=ZNsPYmkSPeI",
+    "cartoon": "https://www.youtube.com/watch?v=K4DyBUG242c&list=PLRBp0Fe2Gpgn8Y9qI-p0aTxVtw8onBSFj"
+    
+}
